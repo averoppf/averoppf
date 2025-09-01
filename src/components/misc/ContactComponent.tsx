06730ac1dbc2d0247e9568ecfa465703fe87891a
mitchellheaney <mitchell.heaney4@gmail.com>
2025-09-01 21:40:42 +1000

    retrigger deployment

--- src/components/misc/ContactComponent.tsx
+++ src/components/misc/ContactComponent.tsx
@@ -96,6 +96,7 @@ export default function ContactComponent() {
           alert("An error occured")
       }
   }, [input])
+  //
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 gap-10">
