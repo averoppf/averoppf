7c34530f9a62cb591133841720730da33c42d1bd
mitchellheaney <mitchell.heaney4@gmail.com>
2025-09-26 14:13:59 +1000

    a

--- src/pages/services/colourWraps.tsx
+++ src/pages/services/colourWraps.tsx
@@ -34,26 +34,20 @@ export function ColourWrapsPage() {
           <meta name="description" content={"Protect your car with Sydney’s car care experts in PPF & ceramic coating. Get unbeatable shine, increased vehicle value & long-lasting protection. Trusted by locals across NSW."} />
       </Helmet> */}
       {/* Hero Section */}
-      <section className="relative lg:h-[calc(90vh)] sm:h-full flex items-center justify-center bg-cover bg-center bg-[url('/squidgie.avif')] py-[5%]">
-          <div className="absolute inset-0 bg-black/80 h-full"></div>
-          <div className="relative z-10 text-center max-w-6xl flex flex-col items-center mx-auto px-6 pt-[15vh]">
-              <motion.h1
-                  ref={titleRef}
-                  animate={titleAnimationControls}
-                  variants={textVariants}
-                  initial="hidden"
-                  transition={{ delay: 0.4, duration: 0.4 }}
-                  className="text-4xl sm:text-6xl md:text-7xl font-bold mb-6 tracking-wider flex flex-col items-center mt-15">
-                  <p className="text-lg w-full text-neutral-400 font-michroma">Services</p>
-                  <p className="block text-white font-michroma mb-[5%] text-4xl sm:text-5xl">Coloured Wraps In Sydeny</p>
-                  {/* <p className="mb-[3%] text-neutral-300 font-light tracking-normal text-base sm:text-lg">At Avero, your vehicle is in expert hands. Our ceramic coating technicians are trained by leaders in automotive surface protection, ensuring consistent, high-standard application on every job. Using high-grade ceramic products and professional techniques, we apply the coating in a clean, controlled environment — allowing the formula to bond correctly with your paintwork or PPF. The result: a durable, ultra-slick finish that resists the elements and enhances visual depth.</p> */}
-                  <a
-                      className="block text-black bg-white font-michroma text-[0.7rem] sm:text-[0.9rem] tracking-[0.15rem] border-2 px-6 py-4 transition-opacity hover:opacity-60 hover:cursor-pointer"
-                      href="/contact"
-                  >ENQUIRE NOW</a>
-              </motion.h1>
-          </div>
-      </section>
+      <motion.h1
+          ref={titleRef}
+          animate={titleAnimationControls}
+          variants={textVariants}
+          initial="hidden"
+          transition={{ delay: 0.4, duration: 0.4 }}
+          className="mt-[25vh] text-4xl sm:text-6xl md:text-7xl font-bold mb-6 tracking-wider flex flex-col items-center justify-center">
+          <p className="text-lg w-full text-neutral-400 font-michroma text-center">Services</p>
+          <p className="block text-white font-michroma mb-[5%] text-4xl sm:text-5xl">Coloured Wraps In Sydney</p>
+          <a
+              className="block text-black bg-white font-michroma text-[0.7rem] sm:text-[0.9rem] tracking-[0.15rem] border-2 px-6 py-4 transition-opacity hover:opacity-60 hover:cursor-pointer"
+              href="/contact"
+          >ENQUIRE NOW</a>
+      </motion.h1>
 
       <section
           ref={containerRef}
