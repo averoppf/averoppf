50d38d962161495f603aa3008b51cc5cc34e2221
averoppf <info@averoppf.com.au>
2026-01-23 12:36:24 +1100

    Update VideoOverlay.tsx

--- src/components/misc/VideoOverlay.tsx
+++ src/components/misc/VideoOverlay.tsx
@@ -20,7 +20,7 @@ export function VideoHero({
       />
 
       {/* Dark gradient */}
-      <div className="absolute inset-x-0 top-0 h-1/2 bg-gradient-to-b from-black/30 to-transparent" />
+      <div className="absolute inset-x-0 top-0 h-full bg-gradient-to-b from-black/20 to-transparent" />
 
       {/* Heading + features */}
       {
