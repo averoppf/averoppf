ea6dae02cbe53e9cb89f25b424c6752ad2033c8a
averoppf <info@averoppf.com.au>
2026-02-04 15:57:42 +1100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import { AreaServed } from './pages/AreaServed'
 import { ColourWrapsPage } from './pages/services/colourWraps'
 import { WindowTintingPage } from './pages/services/windowTinting'
 import ProjectsPage from './pages/projects'
+import TeslaPPFPage from './pages/tesla/tesla-ppf'
 
 function App() {
   return (
@@ -24,6 +25,7 @@ function App() {
         <Route path="/services/ppf" element={<PageLayout><PpfPage /></PageLayout>} />
         <Route path="/services/colour-wraps" element={<PageLayout><ColourWrapsPage /></PageLayout>} />
         <Route path="/services/window-tinting" element={<PageLayout><WindowTintingPage /></PageLayout>} />
+        <Route path="/tesla-ppf" element={<PageLayout><TeslaPPFPage /></PageLayout>} />
         <Route path="/privacy-policy" element={<PageLayout><PrivacyPolicyPage /></PageLayout>} />
         <Route path="*" element={<PageLayout><HomePage /></PageLayout>} />
         <Route path="/services/:area" element={<PageLayout><AreaServed /></PageLayout>} />
