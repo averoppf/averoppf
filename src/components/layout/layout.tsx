8449c4a02ba85e565c2b310ade1aeb2a1bf536c1
averoppf <info@averoppf.com.au>
2026-06-14 12:29:22 +1000

    Update layout.tsx

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,17 +1,29 @@
-import { Banner } from "./Banner";
 import { Footer } from "./footer";
 import { Header } from "./header";
-
 export function PageLayout(props: { children: React.ReactNode }) {
     return (
         <div className="min-h-screen overflow-hidden">
             <Header />
-            <Banner />
             <div className="bg-black min-h-dvh">
                 {props.children}
             </div>
             <Footer />
-            <a href="/contact" className="fixed bottom-[3%] right-[2%] bg-neutral-500 text-white font-michroma font-bold px-4 py-2 border border-neutral-600 rounded-full shadow-lg hover:opacity-80 transform transition-transform duration-200 ease-in-out hover:-translate-y-2 z-50">Get A Quote</a>
+
+                className="
+                fixed 
+                bottom-[3%] right-[2%] 
+                bg-neutral-500 text-white font-michroma font-bold
+                px-4 py-2 
+                border border-neutral-600
+                rounded-full 
+                shadow-lg 
+                hover:opacity-80 transform transition-transform duration-200 ease-in-out hover:-translate-y-2
+                z-50
+                "
+                href="/contact"
+            >
+                Get A Quote
+            </a>
         </div>
     )
 }
