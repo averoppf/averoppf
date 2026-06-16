1be51e38e122e61197b45dad869828e42940e428
averoppf <info@averoppf.com.au>
2026-06-16 18:05:02 +1000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,15 @@ import { ColourWrapsPage } from './pages/services/colourWraps'
 import { WindowTintingPage } from './pages/services/windowTinting'
 import ProjectsPage from './pages/projects'
 import TeslaPPFPage from './pages/tesla/tesla-ppf'
+import ArticlesPage from './pages/articles'
+import WhatIsPPFArticle from './pages/articles/what-is-ppf'
+import PPFvsCeramicArticle from './pages/articles/ppf-vs-ceramic-coating'
+import PPFCostSydneyArticle from './pages/articles/ppf-cost-sydney'
+import PPFCareArticle from './pages/articles/ppf-care-guide'
+import IsPPFWorthItArticle from './pages/articles/is-ppf-worth-it'
+import SelfHealingPPFArticle from './pages/articles/self-healing-ppf'
+import NewCarPPFArticle from './pages/articles/new-car-ppf'
+import PPFMythsArticle from './pages/articles/ppf-myths-debunked'
 
 function App() {
   return (
@@ -27,6 +36,15 @@ function App() {
         <Route path="/services/window-tinting" element={<PageLayout><WindowTintingPage /></PageLayout>} />
         <Route path="/tesla-ppf" element={<PageLayout><TeslaPPFPage /></PageLayout>} />
         <Route path="/privacy-policy" element={<PageLayout><PrivacyPolicyPage /></PageLayout>} />
+        <Route path="/articles" element={<PageLayout><ArticlesPage /></PageLayout>} />
+        <Route path="/articles/what-is-ppf" element={<PageLayout><WhatIsPPFArticle /></PageLayout>} />
+        <Route path="/articles/ppf-vs-ceramic-coating" element={<PageLayout><PPFvsCeramicArticle /></PageLayout>} />
+        <Route path="/articles/ppf-cost-sydney" element={<PageLayout><PPFCostSydneyArticle /></PageLayout>} />
+        <Route path="/articles/ppf-care-guide" element={<PageLayout><PPFCareArticle /></PageLayout>} />
+        <Route path="/articles/is-ppf-worth-it" element={<PageLayout><IsPPFWorthItArticle /></PageLayout>} />
+        <Route path="/articles/self-healing-ppf" element={<PageLayout><SelfHealingPPFArticle /></PageLayout>} />
+        <Route path="/articles/new-car-ppf" element={<PageLayout><NewCarPPFArticle /></PageLayout>} />
+        <Route path="/articles/ppf-myths-debunked" element={<PageLayout><PPFMythsArticle /></PageLayout>} />
         <Route path="*" element={<PageLayout><HomePage /></PageLayout>} />
         <Route path="/services/:area" element={<PageLayout><AreaServed /></PageLayout>} />
       </Routes>
